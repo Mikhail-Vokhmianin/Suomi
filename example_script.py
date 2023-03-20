9ede1930efea71062497b145cb1d37b35edbc364
Mikhail Vokhmianin <35491530+Mikhail-Vokhmianin@users.noreply.github.com>
2023-03-20 18:55:08 +0200

    Update example_script.py

--- example_script.py
+++ example_script.py
@@ -3,4 +3,9 @@ import requests
 
 greets('World')
 
-textdata = requests.get(API)
+r = requests.post("https://api.sentino.org/api/score/text",
+  headers={'Authorization': 'Token 0df3b4f908b73cd3db94914e1df768f79f9331e0'},
+  json={"text": "I am extraverted", "inventories": ["big5", "neo"]},
+)
+
+print("results:", r.json())
